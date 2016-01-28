77fd33c6b1980a02cac004dbdc19e3c1ab36b57e
Amal Ibrahim <Amool@amals-macbook-pro.local>
2016-01-28 11:20:49 +0300

    estimote framework added + alamofire

---
+++ ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  InformME
+//
+//  Created by Amal Ibrahim on 1/28/16.
+//  Copyright © 2016 King Saud University. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
