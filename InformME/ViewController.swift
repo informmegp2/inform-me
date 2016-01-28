2215e279ca00a446e5292d33bab1f47fb77a64ab
Amal Ibrahim <Amool@amals-macbook-pro.local>
2016-01-28 14:15:10 +0300

    This is the modified version of the project: * json is received and printed. *estimote is integrated.

--- InformME/ViewController.swift
+++ InformME/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 /*Hello : ) */
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hi")
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request(.GET, "http://bemyeyes.co/Service.php")
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
