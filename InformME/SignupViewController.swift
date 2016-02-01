a235ef124a3d0092183fa8995de909132edd8480
Amal Ibrahim <Amool@amals-macbook-pro.local>
2016-02-02 01:58:56 +0300

    Signup view added

---
+++ InformME/SignupViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignupViewController.swift
+//  InformME
+//
+//  Created by Amal Ibrahim on 2/2/16.
+//  Copyright © 2016 King Saud University. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    /*Hello : ) */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
