28c3f39382dad8c85c881e5e36bb3a792639d80e
Amal Ibrahim <Amool@amals-macbook-pro.local>
2016-02-05 14:55:04 +0300

    adding tab bar and its view controllers

---
+++ InformME/ManageBeaconsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ManageBeaconsViewController.swift
+//  InformME
+//
+//  Created by Amal Ibrahim on 2/5/16.
+//  Copyright © 2016 King Saud University. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class ManageBeaconsViewController: UIViewController {
+    /*Hello : ) */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //setup tint color for tha back button.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+
+}
---
+++ InformME/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  InformME
+//
+//  Created by Amal Ibrahim on 2/5/16.
+//  Copyright © 2016 King Saud University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    /*Hello : ) */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //setup tint color for tha back button.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+
+}
