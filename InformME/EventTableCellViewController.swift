8193723fb748ee547d2b2a74f112c68bef3b4c20
Amal Ibrahim <Amool@Amals-MacBook-Pro.local>
2016-02-05 21:16:30 +0300

    Tabbar connected to login page

---
+++ InformME/EventTableCellViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EventTableCell.swift
+//  InformME
+//
+//  Created by Amal Ibrahim on 2/5/16.
+//  Copyright © 2016 King Saud University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventTableCellViewController: UITableViewCell {
+    
+}
