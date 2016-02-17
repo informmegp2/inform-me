209d565963adf3e90e63da6a678d964e3237a960
Amal <amal.alabdulkarim@gmail.com>
2016-02-17 22:46:38 +0300

    red files issue

---
+++ InformME/ContentDetailsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ContentDetailsViewController.swift
+//  InformME
+//
+//  Created by Amal Ibrahim on 2/17/16.
+//  Copyright © 2016 King Saud University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ContentDetailsViewController: UIViewController {
+}
