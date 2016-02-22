dba675ee437e7bbd5ff523326cb24f3ce8138005
Amal <amal.alabdulkarim@gmail.com>
2016-02-22 21:22:47 +0300

    attendee contents partially done :)

---
+++ InformME/ContentForAttendeeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ContentForAttendeeViewController.swift
+//  InformME
+//
+//  Created by Amal Ibrahim on 2/22/16.
+//  Copyright © 2016 King Saud University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContentForAttendeeViewController: UIViewController {
+    
+}
