5dd78c6d42193f5348a2b84c70e00238a5c81337
Amal <amal.alabdulkarim@gmail.com>
2016-02-26 21:13:59 +0300

    organizer profile logout

---
+++ InformME/ProfileOrganizerViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ProfileOrganizerViewController.swift
+//  InformME
+//
+//  Created by Amal Ibrahim on 2/26/16.
+//  Copyright © 2016 King Saud University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ProfileOrganizerViewController : UIViewController {
+    /*Hello : ) */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //setup tint color for tha back button.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func out(sender: AnyObject) {
+        print(" iam in 1")
+        
+        var flag: Bool
+        flag = false
+        
+        
+        
+        var current: Authentication = Authentication();
+        
+        current.logout(){
+            (login:Bool) in
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                flag = login
+                if(flag) {
+                    
+                    self.performSegueWithIdentifier("backtologin", sender: self)
+                    
+                    
+                    print("I am happy",login,flag) }
+                
+            }
+            print("I am Here")  }
+        
+        
+        
+        
+        
+        
+    } //end out */ backtologin
+    
+    
+    
+    
+    
+    
+}
