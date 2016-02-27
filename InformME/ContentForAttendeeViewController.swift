1d8e6a0114f6ff4614e235e66b9471badfa43d79
nouf abdullah <noufabdullah@noufs-macbook-pro.local>
2016-02-27 18:02:59 +0300

    share content via twitter

--- InformME/ContentForAttendeeViewController.swift
+++ InformME/ContentForAttendeeViewController.swift
@@ -8,11 +8,50 @@
 
 import Foundation
 import UIKit
+import Social
 
 class ContentForAttendeeViewController: UIViewController {
     
+    func showAlertMessage(message: String!) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "موافق", style: UIAlertActionStyle.Default, handler: nil))
+        presentViewController(alertController, animated: true, completion: nil)
+    }
     
     
+    @IBAction func shareContent(sender: AnyObject) {
+        
+        
+       let actionSheet = UIAlertController(title: "", message: "انشر المحتوى عبر", preferredStyle: UIAlertControllerStyle.ActionSheet)
+       
+       
+        let tweetAction = UIAlertAction(title: "تويتر", style: UIAlertActionStyle.Default) { (action) -> Void in
+            if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+         let twitterComposeVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+                twitterComposeVC.setInitialText("test code")
+                self.presentViewController(twitterComposeVC, animated: true, completion: nil)
+
+                
+            }
+            else {
+               self.showAlertMessage("يجب عليك أولًا تسجيل الدخول بتويتر")
+            }
+        }
+    
+        
+        let dismissAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (action) -> Void in
+            
+        }
+        
+        
+        actionSheet.addAction(tweetAction)
+       
+        actionSheet.addAction(dismissAction)
+        
+        presentViewController(actionSheet, animated: true, completion: nil)
+        
+        
+    }
     
     @IBAction func out(sender: AnyObject) {
         print(" iam in 1")
