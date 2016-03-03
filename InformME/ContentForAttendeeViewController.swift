55cd8c9cdf518bfc741477bb53f5c3103128c2e1
nouf abdullah <noufabdullah@noufs-macbook-pro.local>
2016-03-03 13:41:02 +0300

    like and dislike

--- InformME/ContentForAttendeeViewController.swift
+++ InformME/ContentForAttendeeViewController.swift
@@ -19,7 +19,7 @@ class ContentForAttendeeViewController: UIViewController,  UITableViewDelegate, 
     @IBOutlet weak var navbar: UINavigationItem!
     @IBOutlet weak var commentField: UITextField!
     var content: Content = Content()
-    var cid: Int = 140
+    var cid: Int = 106
     var uid: Int = 29
     
     @IBOutlet var likeButton: UIButton!
@@ -159,32 +159,50 @@ class ContentForAttendeeViewController: UIViewController,  UITableViewDelegate, 
     @IBOutlet var dislikeButton: UIButton!
     
     @IBAction func likeContent(sender: AnyObject) {
-          if (self.content.like==0 || self.content.dislike==0){
+          if (self.content.like==0 && self.content.dislike==0){
         self.content.likeContent(self.cid, uid: self.uid){
             (done:Bool) in
             dispatch_async(dispatch_get_main_queue()) {
                 print("I am cool")
       self.likeButton.setImage(UIImage(named: "like.png"), forState: UIControlState.Normal)
                 self.content.like=1
+                print(self.content.like)
+                print(self.content.dislike)
             }
             }}
+          else if (self.content.dislike==1){
+            self.likeButton.setImage(UIImage(named: "like.png"), forState: UIControlState.Normal)
+            self.content.like=1
+            self.content.dislike=0
+            self.dislikeButton.setImage(UIImage(named: "dislike-g.png"), forState: UIControlState.Normal)
+            print(self.content.like)
+            print(self.content.dislike)
+        }
 
     }
     
     
     
     @IBAction func dislikeContent(sender: AnyObject) {
-        if (self.content.like==0 || self.content.dislike==0){
+        if (self.content.like==0 && self.content.dislike==0){
         self.content.disLikeContent(self.cid, uid: self.uid){
             (done:Bool) in
             dispatch_async(dispatch_get_main_queue()) {
                 print("I am cool")
                  self.dislikeButton.setImage(UIImage(named: "dislike.png"), forState: UIControlState.Normal)
                 self.content.dislike=1
-                
+                print(self.content.like)
+                print(self.content.dislike)
             }}
         }
-
+        else if (self.content.like==1){
+            self.dislikeButton.setImage(UIImage(named: "dislike.png"), forState: UIControlState.Normal)
+            self.content.like=0
+            self.content.dislike=1
+            self.likeButton.setImage(UIImage(named: "like-g.png"), forState: UIControlState.Normal)
+            print(self.content.like)
+            print(self.content.dislike)
+        }
     }
     
     //MARK: --- New Comment --- 
